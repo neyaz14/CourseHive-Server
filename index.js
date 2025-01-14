b185d11d50d2c8847ed0d1ed4a0eb5b246962279
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-14 18:20:47 +0600

    jwt related bug fixed

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const jwt = require('jsonwebtoken')
-const morgan = require('morgan')
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000
-const app = express()
 // middleware
-const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
-  credentials: true,
-  optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan('dev'))
-
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-
-  if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
-    }
-    req.user = decoded
-    next()
-  })
-}
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mq0mae1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+app.use(cors());
+app.use(express.json());
+// 
+//
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epj76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -44,74 +19,76 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
-})
+  }
+});
+
 async function run() {
   try {
-    // database 
-    const db = client.db('courseHive');
-    const usersCollection = db.collection('users');
-    // Generate jwt token
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("courseHive").collection("users");
+    
+
+    // jwt related api
     app.post('/jwt', async (req, res) => {
-      const email = req.body
-      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '365d',
-      })
-      res
-        .cookie('token', token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        })
-        .send({ success: true })
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
     })
-    // Logout
-    app.get('/logout', async (req, res) => {
-      try {
-        res
-          .clearCookie('token', {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-          })
-          .send({ success: true })
-      } catch (err) {
-        res.status(500).send(err)
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
       }
-    })
-    // save user 
-    // save or update a user in db
-    app.post('/users/:email', async (req, res) => {
-      const email = req.params.email
-      const query = { email }
-      const user = req.body
-      // check if user exists in db
-      const isExist = await usersCollection.findOne(query)
-      if (isExist) {
-        return res.send(isExist)
-      }
-      const result = await usersCollection.insertOne({
-        ...user,
-        role: 'student',
-        timestamp: Date.now(),
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
       })
-      res.send(result)
-    })
+    }
+
+
+    // users related api
+    app.get('/users', verifyToken, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    
+
     // Send a ping to confirm a successful connection
-    // await client.db('admin').command({ ping: 1 })
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello from CourseNet Server..')
+  res.send('boss is sitting')
 })
 
 app.listen(port, () => {
-  console.log(`CourseNet is running on port ${port}`)
+  console.log(`Bistro boss is sitting on port ${port}`);
 })

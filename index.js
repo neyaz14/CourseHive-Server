9a51d21371fb494a5d79b932f1dd843bbdc0875d
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-20 23:02:57 +0600

    facing deployment realted prblms

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ const port = process.env.PORT || 5000;
 //   credentials: true,
 //   optionSuccessStatus: 200,
 // }
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','https://simple-firebase-4327b.web.app'],
-  credentials: true,
-  optionSuccessStatus: 200,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 // 
